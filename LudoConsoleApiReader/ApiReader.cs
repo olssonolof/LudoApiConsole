412a12836ce2437a145eb554403d748f972a4be7
olssonolof <44023615+olssonolof@users.noreply.github.com>
2019-01-28 13:42:18 +0100

    First commit.

---
+++ LudoConsoleApiReader/ApiReader.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using Newtonsoft.Json;
+
+namespace LudoConsoleApiReader
+{
+    static public class ApiReader
+    {
+
+        public static List<string> NewGame(RestClient url, string numberOfPlayers, string gameName)
+        {
+            var jsonDeserializer = new JsonDeserializer();
+
+
+            var request = new RestRequest("creategame/", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            url.AddHandler("application/json", jsonDeserializer);
+            request.AddJsonBody(new string[] { numberOfPlayers, gameName, "1" });
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = url.Execute(request);
+            string answer = response.Content;
+
+            var answerArray = JsonConvert.DeserializeObject<List<string>>(answer);
+
+            return answerArray;
+
+        }
+
+        public static List<string> ViewSavedGames(RestClient url)
+        {
+            var jsonDeserializer = new JsonDeserializer();
+
+
+            var request = new RestRequest("games/", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            url.AddHandler("application/json", jsonDeserializer);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = url.Execute(request);
+            string answer = response.Content;
+
+            var answerList = JsonConvert.DeserializeObject<List<string>>(answer);
+
+            return answerList;
+
+        }
+
+        public static List<string> MovePiece(RestClient url, string PieceNr)
+        {
+            var jsonDeserializer = new JsonDeserializer();
+
+
+            var request = new RestRequest("updatepieceposition/" + PieceNr, Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            url.AddHandler("application/json", jsonDeserializer);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = url.Execute(request);
+            string answer = response.Content;
+
+            var answerList = JsonConvert.DeserializeObject<List<string>>(answer);
+
+            return answerList;
+
+        }
+
+        public static string SaveGame(RestClient url)
+        {
+            var jsonDeserializer = new JsonDeserializer();
+
+
+            var request = new RestRequest("savegame/", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            url.AddHandler("application/json", jsonDeserializer);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = url.Execute(request);
+            string answer = response.Content;
+
+            var answerList = JsonConvert.DeserializeObject<string>(answer);
+
+            return answerList;
+        }
+
+        public static List<string> LoadGame(RestClient url, string gameName)
+        {
+            var jsonDeserializer = new JsonDeserializer();
+
+
+            var request = new RestRequest("loadgame/"+gameName, Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            url.AddHandler("application/json", jsonDeserializer);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = url.Execute(request);
+            string answer = response.Content;
+
+            var answerList = JsonConvert.DeserializeObject<List<string>>(answer);
+
+            return answerList;
+
+        }
+
+        public static List<string> ShowGameInfo(RestClient url)
+        {
+            var jsonDeserializer = new JsonDeserializer();
+
+
+            var request = new RestRequest("gameinfo/", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            url.AddHandler("application/json", jsonDeserializer);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = url.Execute(request);
+            string answer = response.Content;
+
+            var answerList = JsonConvert.DeserializeObject<List<string>>(answer);
+
+            return answerList;
+        }
+    }
+}
---
+++ LudoConsoleApiReader/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace LudoConsoleApiReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://localhost:53546/api/ludo/Game/");
+            string nrOfPlayer = "";
+            string gameName = "";
+            List<string> answer = new List<string>();
+
+            int choice = ReadInt("1: New game?\n2: Load game?");
+
+
+
+            if (choice == 1)
+            {
+                nrOfPlayer = ReadString("How many players?");
+                gameName = ReadString("What du you want to call Your game?");
+                answer = ApiReader.NewGame(client, nrOfPlayer, gameName);
+            }
+            else
+            {
+                answer = ApiReader.ViewSavedGames(client);
+                for (int i = 0; i < answer.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {answer[i]}");
+                }
+
+
+
+                answer = ApiReader.LoadGame(client, answer[ReadInt("What game do You want to load?") - 1]);
+                Console.Clear();
+
+            }
+
+            while (true)
+            {
+                List<string> games = GameInfo(client);
+                for (int i = 0; i < games.Count - 1; i++)
+                {
+                    Console.WriteLine(games[i]);
+                }
+
+                Console.WriteLine($"The dice showed {games[games.Count - 1].Substring(0, games[games.Count - 1].IndexOf(','))}.\nIt is {games[games.Count - 1].Substring(games[games.Count - 1].IndexOf(',') + 1)}'s turn to move.");
+                answer = ApiReader.MovePiece(client, ReadString("What piece do You want to move?"));
+                Console.Clear();
+                Console.WriteLine($"Player {answer[0]}.{answer[1]}.");
+
+                if (ReadString("To save, press y") != "")
+                {
+                    string save = ApiReader.SaveGame(client);
+                    Console.WriteLine(save);
+                }
+
+            }
+
+        }
+
+        static int ReadInt(string promt)
+        {
+            Console.WriteLine(promt);
+            return int.Parse(Console.ReadLine());
+
+        }
+
+        static string ReadString(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+
+        static List<string> GameInfo(RestClient url)
+        {
+            return ApiReader.ShowGameInfo(url);
+        }
+
+
+
+    }
+}
